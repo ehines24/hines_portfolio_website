d5c4f4b2b861784a3dc15d59ad19d20db920a7ec
Eddie <ehines@umass.edu>
2024-12-05 17:25:31 -0500

    Simple name, image and header

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HeaderLogo from './HeaderLogo/HeaderLogo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HeaderLogo />
   </React.StrictMode>
 );
 
