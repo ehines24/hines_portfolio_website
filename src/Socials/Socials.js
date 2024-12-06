9ba977b9f8871b1ecf878b54a73a0ea084352bbb
Eddie <ehines@umass.edu>
2024-12-06 18:12:06 -0500

    Added resume and icon to click on to take you to resume

--- src/Socials/Socials.js
+++ src/Socials/Socials.js
@@ -1,12 +1,14 @@
 import './Socials.css';
 import github from "./github-mark.png"
 import linkedin from "./LI-In-Bug.png";
+import cv_icon from './cv_icon.png';
 
 function Socials() {
   return (
     <div class="SocialBox">
         <a href='https://www.linkedin.com/in/edward-g-hines/'><img alt="LinkedIn logo that navigates the user to Edward Hines' LinkedIn page" src={linkedin} class="SocialLogo"/></a>     
         <a href='https://www.github.com/ehines24/'><img alt="GitHub logo that navigates the user to Edward Hines' GitHub page" src={github} class="SocialLogo"/></a>
+        <a href="/resume82024.pdf"><img alt="Link to general resume" class="SocialLogo" src={cv_icon}/> </a>
     </div>
   );
 }
