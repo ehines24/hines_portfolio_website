470c2fc71c6a8b4d89b8680e429aac822db87040
Eddie <ehines@umass.edu>
2024-12-06 15:22:05 -0500

    Added executive summary, pinned socials at bottom

--- src/Socials/Socials.js
+++ src/Socials/Socials.js
@@ -5,8 +5,8 @@ import linkedin from "./LI-In-Bug.png";
 function Socials() {
   return (
     <div class="SocialBox">
-      <a href='https://www.linkedin.com/in/edward-g-hines/'><img alt="LinkedIn logo that navigates the user to Edward Hines' LinkedIn page" src={linkedin} class="SocialLogo"/></a>     
-      <a href='https://www.github.com/ehines24/'><img alt="GitHub logo that navigates the user to Edward Hines' GitHub page" src={github} class="SocialLogo"/></a>
+        <a href='https://www.linkedin.com/in/edward-g-hines/'><img alt="LinkedIn logo that navigates the user to Edward Hines' LinkedIn page" src={linkedin} class="SocialLogo"/></a>     
+        <a href='https://www.github.com/ehines24/'><img alt="GitHub logo that navigates the user to Edward Hines' GitHub page" src={github} class="SocialLogo"/></a>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HeaderLogo from './HeaderLogo/HeaderLogo';
-import Socials from './Socials/Socials';
+import MainBody from './MainBody/MainBody';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HeaderLogo />
-    <Socials />
+    <MainBody/>
   </React.StrictMode>
 );
 
