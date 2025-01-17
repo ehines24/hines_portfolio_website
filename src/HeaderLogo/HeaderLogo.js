6dbe376bf780aedc506e53bf0d6660e710a941ab
Eddie <ehines@umass.edu>
2025-01-17 16:11:59 -0500

    Removed redundance from alt text, changed resume

--- src/HeaderLogo/HeaderLogo.js
+++ src/HeaderLogo/HeaderLogo.js
@@ -4,7 +4,7 @@ import profile from "./headshot.jpg";
 function HeaderLogo() {
   return (
     <div className="HeaderLogo">
-      <div className="Logo"><img src={profile}  alt='Image of Edward Hines, dressed professionally, smile on face, wearing a blue shirt against a blue background.'/>
+      <div className="Logo"><img src={profile}  alt='Edward Hines, dressed professionally, smile on face, wearing a blue shirt against a blue background.'/>
     </div>
             <div className="header">Edward Hines</div> 
     </div>
--- src/Socials/Socials.js
+++ src/Socials/Socials.js
@@ -4,11 +4,12 @@ import linkedin from "./LI-In-Bug.png";
 import cv_icon from './cv_icon.png';
 
 function Socials() {
+  let current_resume = "/resume12025.pdf"
   return (
     <div class="SocialBox">
         <a href='https://www.linkedin.com/in/edward-g-hines/'><img alt="LinkedIn logo that navigates the user to Edward Hines' LinkedIn page" src={linkedin} class="SocialLogo"/></a>     
         <a href='https://www.github.com/ehines24/'><img alt="GitHub logo that navigates the user to Edward Hines' GitHub page" src={github} class="SocialLogo"/></a>
-        <a href="/resume82024.pdf"><img alt="Link to general resume" class="SocialLogo" src={cv_icon}/> </a>
+        <a href={current_resume}><img alt="Link to general resume" class="SocialLogo" src={cv_icon}/> </a>
     </div>
   );
 }
