a89c8a089ba825d404c27403b9fad5e95142975c
Eddie <ehines@umass.edu>
2025-09-19 12:40:34 -0400

    Corrected GPA

--- src/MainBody/MainBody.js
+++ src/MainBody/MainBody.js
@@ -4,7 +4,7 @@ function MainBody() {
     return (
     <div class="BodyFlex">
         <div class="ExecutiveSummary">
-            <div class="SummaryText">Driven computer science and math graduate with a 3.8 GPA. Passionate about building web applications using the MERN stack, diving deep into the Linux operating system and embedded systems using the C programming language, and delivering tech solutions to others in the role of a tech educator. Worked with agile, diverse software teams to deliver satisfactory results for every stakeholder. </div>
+            <div class="SummaryText">Driven computer science and math graduate with a 3.7 GPA. Passionate about building web applications using the MERN stack, diving deep into the Linux operating system and embedded systems using the C programming language, and delivering tech solutions to others in the role of a tech educator. Worked with agile, diverse software teams to deliver satisfactory results for every stakeholder. </div>
         </div>
         <Socials/>
      </div> 
